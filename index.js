4c64009178265554fff89a2c45d9ab44d75386a7
Brian Guzmán <b.guzman.m@me.com>
2022-12-03 14:01:23 -0300

    tercereichon

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const { MongoClient, ObjectId } = require('mongodb'); // hago require del driver de MongoDB
 require('dotenv').config();
 
+app.use(express.json()); // habilita que el req.body lea los datos en json
+
 const uri = 'mongodb://localhost:27017';
 
 const getPersons = async () => {
@@ -59,7 +61,53 @@ const getPersonById = async (id) => {
   return person;
 };
 
+//const addPerson = async (person) => {
+const addPerson = async (name, email, country, birthday, company, marital_status) => {
+  const client = new MongoClient(uri);
 
+  // let person = {
+  //   name: name,
+  //   email: email,
+  //   country: country,
+  //   birthday: birthday,
+  //   company: company,
+  //   marital_status: marital_status
+  // }
+
+  try {
+    await client.db('db_persons').collection('persons').insertOne(
+      {
+        name: name,
+        email: email,
+        country: country,
+        birthday: birthday,
+        company: company,
+        marital_status: marital_status
+      }
+    );
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.close();
+  }
+};
+
+const delPerson = async (id) => {
+  const filter = {
+    "_id": ObjectId(id)
+  }
+  const client = new MongoClient(uri);
+  try {
+    await client.db("db_persons").collection("persons").deleteOne(filter);
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await client.close();
+  }
+};
+
+
+// end points
 app.get('/persons', (req, res) => {
   getPersons()
     .then((data) => {
@@ -80,6 +128,22 @@ app.get('/persons/bycountry/:country', (req, res) => {
     })
 });
 
+app.post('/persons', (req, res) => {
+  addPerson(req.body.name, req.body.email, req.body.country, req.body.birthday, req.body.company, req.body.marital_status)
+    .then(console.log('document is created'));
+  res.send("ok");
+});
+
+app.delete('/persons/:id', (req, res) => {
+  delPerson(req.params.id)
+    .then(() => {
+      console.log("document is deleted");
+      res.send("ok");
+    });
+  // .catch(
+  //   res.send("no ok")
+  // );
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`listen in port ${process.env.PORT}`);
